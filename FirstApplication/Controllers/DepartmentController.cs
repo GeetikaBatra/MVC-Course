99f0a434732cf06dd1d75c8c0fefc0e8bf617d22
Geetika Batra <geetika791@gmail.com>
2014-11-19 13:49:37 +0530

    new Mvc Course

---
+++ FirstApplication/Controllers/DepartmentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    //This is we defininig a controller named department,but on browser if we dont mention the name department,then it
+    //looks for a default controller,this default controller is the home controller which is a new controller to be added in the controller section
+    //in the solution explorer.
+    //though we can change it i.e make department controller as default controller and get info method as default action
+    //Just go to app_start file and replace default controller from home to department and default action from index to getinfo.
+
+    //Introduction to Controller
+    public class DepartmentController : Controller//departmentcontroller is a class derived from class controller already in it,it contains methods
+    {
+        // GET: Department
+        //But for this to work we have to cooment out above index which is of type action result,otherwise the browser address will get confused 
+        //which one to run
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string GetInfo()//controller means a class and action means method
+        {
+            return "Welcome ToString my FirstApplication MVC view";
+        }
+        public string GetInfoWithDate()
+        {
+            return "Welcome to my first MVC program " + DateTime.Now.ToString();
+        }
+        public string index()//This is bydefault action i.e if we donot mention the action in a webpage this method will execute defaultly.
+        {
+            return "Welcome";
+        }
+    }
+}
---
+++ FirstApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApplication.Controllers
+{
+    //if we donot mention any controller,then by default home controller is accessed and method index is accessed .This is because in app_start 
+    //in route map default controller is home and default action is index
+    public class HomeController : Controller
+    {
+       public string Index()
+        {
+            return "Welcome to Mvc Series from home Index";
+        }
+    }
+}
