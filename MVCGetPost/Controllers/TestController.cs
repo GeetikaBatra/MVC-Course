56e787815f61aaada53ed29eabb35a78be241c4a
Geetika Batra <geetika791@gmail.com>
2014-11-22 15:26:01 +0530

    parametric method to extract values

--- MVCGetPost/Controllers/TestController.cs
+++ MVCGetPost/Controllers/TestController.cs
@@ -5,11 +5,17 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace MVCGetPost.Controllers
-{
+{ //WHENEVER we hit this action method,we have values in it
+    //how do we read the values in this method,there are 3 ways to do that
+    //1st -have the data passed as parameters to your action method.
+    //Extract it from a set of context objects.
+    //explicitly invoke the framework's model binding feature.
     public class TestController : Controller
     {
         // GET: Test
-        public ActionResult GotoThisAction()
+        //parameters method 
+        public ActionResult GotoThisAction(string N_Name,int N_Age)
+           
         {
             return View();
         }
