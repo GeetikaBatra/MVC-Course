6b725a3d9aef17151951b2ef759a7e1f7639677d
Geetika Batra <geetika791@gmail.com>
2015-01-06 23:15:03 +0530

    List control using html helper classes

---
+++ HtmlHelperClasses/HtmlHelperClasses/Qual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtmlHelperClasses
+{
+    public class Qual
+    {
+        public int id { get; set; }
+        public string Q { get; set; }
+    }
+}
