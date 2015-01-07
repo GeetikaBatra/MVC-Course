6f2b6b29ec5add66475be15d9701387abd5c14eb
Geetika Batra <geetika791@gmail.com>
2015-01-07 20:57:05 +0530

    ViewData Vs ViewBag in depth

--- ViewdataVsViewBag/ViewdataVsViewBag/Controllers/HomeController.cs
+++ ViewdataVsViewBag/ViewdataVsViewBag/Controllers/HomeController.cs
@@ -16,14 +16,21 @@ namespace ViewdataVsViewBag.Controllers
         }
         public ActionResult Read(int ID)
         {
-            var Name = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault().Name;
-            var Age = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault().Age;
+            //var Name = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault().Name;
+            //var Age = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault().Age;
+            var obj = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault();
+
 
             //ViewData["N"] = Name;
             //ViewData["A"] = Age; Instead of ViewData we can use ViewBag as its syntax is close to C#
 
-            ViewBag.N=Name;
-            ViewBag.A = Age;
+            //ViewBag.N=Name;
+            //ViewBag.A = Age;
+            //ViewData["O"] = obj;
+
+            ViewBag.O = obj;
+
+             
 
             return View("Index");//getting back to index view after being redirected from index view to read
         }
