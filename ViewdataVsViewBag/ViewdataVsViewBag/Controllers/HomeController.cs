2a1c47e4e8b7c7b205603e96ab3c06e9ebedf4d7
Geetika Batra <geetika791@gmail.com>
2015-01-07 19:04:16 +0530

    ViewdataVsViewbag

---
+++ ViewdataVsViewBag/ViewdataVsViewBag/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ViewdataVsViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        GetPostEntities OE = new GetPostEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Read(int ID)
+        {
+            var Name = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault().Name;
+            var Age = OE.tbl_MyTable.Where(p => p.ID == ID).FirstOrDefault().Age;
+
+            ViewData["N"] = Name;
+            ViewData["A"] = Age;
+
+
+            return View("Index");//getting back to index view after being redirected from index view to read
+        }
+
+       
+    }
+}
