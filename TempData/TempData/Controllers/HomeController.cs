838b5566e9c2dc00b7f834bc37352fb06553c53b
Geetika Batra <geetika791@gmail.com>
2015-01-09 16:16:01 +0530

    TempData

---
+++ TempData/TempData/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Insert(FormCollection f)
+        {
+
+            DepartmentEntities OE = new DepartmentEntities();
+            tbl_Department D = new tbl_Department();
+            D.D_Name = f["DName"].ToString();
+            D.HOD = f["HOD"].ToString();
+            OE.tbl_Department.Add(D);
+            OE.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+      
+    }
+}
