d8886f4b299eb95c2b058866c7d32b88910fa4ab
Geetika Batra <geetika791@gmail.com>
2015-02-04 15:25:07 +0530

    form Validation 1

---
+++ ValidartionExample/ValidartionExample/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidartionExample.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage="UserNameRequired")]
+        public string UserName { get; set; }
+        public string Password{ get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Qualification { get; set; }
+        public string Gender { get; set; }
+        public string CurrentCTC { get; set; }
+        public string ExpectedCTC { get; set; }
+        public string Email { get; set; }
+        public string URL { get; set; }
+
+    }
+}
